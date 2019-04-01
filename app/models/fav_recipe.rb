7250a0d7c51d7f6af3553c9315f214586cccd428
Tristan <tristanberezowski@gmail.com>
2019-04-01 22:09:24 +0000

    validations for fav recipe

--- app/models/fav_recipe.rb
+++ app/models/fav_recipe.rb
@@ -3,5 +3,8 @@ class Fav_recipe < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
   has_many :list_ingredients
+  
+  validates :user, presence: true
+  validates :recipe, presence: true
 
 end
