f7a3141ccbb2b297950650770fa64e3f42dc095c
Rachel Gould <rachegould@gmail.com>
2019-04-02 04:08:18 +0000

    Add twilio, getting error on message create

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -5,10 +5,26 @@ class TextController < ApplicationController
     if q.blank?
       render status: 400, json: { error: 'Missing a phone number!'}
     else
+      boot_twilio
+      from = '+16474925440' # Your Twilio number
+      to = '+16474925440' # Should be changed to destination for production
+      sms = @client.messages.create(
+        from: from,
+        to: to,
+        body: "Hey friend!" # Replace this with the current user's grocery list
+      )
       render(
         status: 200,
-        json: User
+        json: { success: 'Message Sent!'}
       )
     end
+  end
+
+  private
+
+  def boot_twilio
+    account_sid = ENV["TWILIO_SID"]
+    auth_token = ENV["TWILIO_AUTH"]
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 end
