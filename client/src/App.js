0425ab170ebd52059ead53620a2f0a5938178a6a
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-02 14:49:45 -0700

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-import React, {
-  Component
-} from 'react';
-// import './styles/foundation.min.css';
-=======
 import React, { Component } from 'react';
-import './styles/foundation.min.css';
->>>>>>> 8e5c607df03ef85c302785dc271d432797ec2f5f
 import  Routes from './routes';
 
 class App extends Component {
