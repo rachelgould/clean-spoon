9dfae83400fa2640fc8af0c60b481ac123ebfe3e
Rachel Gould <rachegould@gmail.com>
2019-04-02 18:24:34 +0000

    pull from master

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import './styles/foundation.min.css';
 import  Routes from './routes';
 
@@ -11,9 +9,6 @@ class App extends Component {
   render() {
     return ( 
     <div>
-      {/* http://grandtheatrecompany.com/wp-content/uploads/2016/05/img_3.jpg */}
-      {/* https://img.freepik.com/free-photo/colorful-healthy-unhealthy-food-white-textured-background_23-2147885658.jpg?size=626&ext=jpg */}
-      {/* <img className='bg' src={'https://images.pexels.com/photos/1640773/pexels-photo-1640773.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'} /> */}
       <Routes />
     </div>
     );
@@ -21,4 +16,3 @@ class App extends Component {
 }
 
 export default App;
-// module.exports = App;
