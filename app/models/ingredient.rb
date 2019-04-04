dc1bd1ba7a3cb3b35dd8dac50d5d5096c7e299e8
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-04 11:55:43 -0700

    add private method to get images before ingredient save as callback

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,16 @@
 class Ingredient < ApplicationRecord
+  before_save :get_image
 
   has_many :list_ingredients
   has_many :fridge_ingredients
 
   validates :name, presence: true
 
+  private
+    def get_image
+      if image.nil?
+        puts "Finding picture for #{name}"
+        
+      end
+    end
 end
