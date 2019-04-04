e5eac8903a102bf84da45eaa1bf39148bc1badac
Tristan Berezowski <tristanberezowski@gmail.com>
2019-04-03 17:11:37 -0700

    list ingredients passed through index

--- app/controllers/list_ingredients_controller.rb
+++ app/controllers/list_ingredients_controller.rb
@@ -1,7 +1,14 @@
 class ListIngredientsController < ApplicationController
 
   def index
-    json_response({msg: "hello"})
+    @user = User.find(params[:userId])
+    @list_ingredients = @user.list_ingredients
+    @ingredients = [];
+    @list_ingredients.each do |i|
+      @ingredients << [Ingredient.select("name, image").find(i)]
+    end
+    # @ingredients
+    json_response(@ingredients)
   end
 
   def create
