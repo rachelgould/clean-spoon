3bba70c9d0a4adf3aa13288139d99df694862de0
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-03 17:07:42 -0700

    extract API calls into separate module

--- client/src/components/ShoppingList/SMSForm.jsx
+++ client/src/components/ShoppingList/SMSForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Input, InputGroup, InputGroupAddon, Button } from 'reactstrap';
-import axios from 'axios';
+import { sendSMS } from '../../lib/api.js'
 
 class SMSForm extends Component {
   constructor(props) {
@@ -28,15 +28,7 @@ class SMSForm extends Component {
     event.stopPropagation();
     let id = this.props.cookies.get('id')
     let phoneNum = this.state.number
-    console.log(`Submitting the text message for user id ${id} to this phone number: ${phoneNum}`)
-    axios.get('/text', {
-      params: {
-        q: phoneNum,
-        id: id
-      }
-    }).then(response => {
-      console.log(response)
-    }).catch(error => console.log(error))
+    sendSMS(id, phoneNum);
   }
 
   render() {
