17c3f5855962b62caa219c37eb7aa7fae64f6f3b
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-03 21:40:41 -0700

    shopping list returns items from API

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 
-// Pings Rails API to send the grocery list for the specified user to the specified phone number
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(`HTTP Error ${response.statusText}`);
+  error.status = response.statusText;
+  error.response = response;
+  console.log(error);
+}
+
 function sendSMS(id, number) {
   console.log(`Submitting the text message for user id ${id} to this phone number: ${number}`)
   axios.get('api/text', {
@@ -14,27 +23,14 @@ function sendSMS(id, number) {
 }
 
 function getShoppingList(id, cb) {
-  let url = `api/lists/${id}`
+  let url = 'api/lists/' + id
   axios.get(url).then(response => {
     console.log('Received shopping list from server')
+    console.log('The response was::::::', response)
+    return response
   })
   .then(checkStatus)
-  .then(parseJSON)
   .then(cb)
-}
-
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  }
-  const error = new Error(`HTTP Error ${response.statusText}`);
-  error.status = response.statusText;
-  error.response = response;
-  console.log(error);
-}
-
-function parseJSON(response) {
-  return response.json();
 }
 
 export { sendSMS, getShoppingList }
