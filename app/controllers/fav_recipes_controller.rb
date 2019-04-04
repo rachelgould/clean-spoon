8f1ed10ce614f673c7d65828dac4e873f8133541
Tristan <tristanberezowski@gmail.com>
2019-04-04 01:10:29 +0000

    fav recipe controller index

--- app/controllers/fav_recipes_controller.rb
+++ app/controllers/fav_recipes_controller.rb
@@ -1,7 +1,17 @@
 class FavRecipesController < ApplicationController
 
   def index
-    json_response({bingo: "bingo"})
+    @user = User.find(params[:userId])
+    recipes = []; 
+    @user.fav_recipes.each do |i|
+      if i.recipe[:yummily_id != nil] 
+        recipes << {
+          name: i.recipe[:name],
+          preparation: i.recipe[:preparation]
+        }
+      end
+    end
+    json_response(recipes)
   end
 
   def create
