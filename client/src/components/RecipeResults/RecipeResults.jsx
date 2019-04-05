5f1dec5b3393b15490cb900ea18f699f6f86ccc8
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-05 16:18:44 -0700

    use sample recipes json object

--- client/src/components/RecipeResults/RecipeResults.jsx
+++ client/src/components/RecipeResults/RecipeResults.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from '../Navbar/nav.jsx';
+import recipeSample from './recipesample';
 
 function RecipeResults(props) {
-  return (<p>Results here</p>)
+  const recipesJSON = JSON.stringify(recipeSample)
+  const [recipes, setRecipes] = useState(recipesJSON);
+
+  return (
+    <div className="recipe-results">
+      <Navbar />
+      <p>{recipes}</p>
+    </div>
+  )
 }
 
 export default RecipeResults;
