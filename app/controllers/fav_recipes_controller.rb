4363ccd70ab4f1da078442bd18a19127b0c462f5
Tristan <tristanberezowski@gmail.com>
2019-04-05 02:06:07 +0000

    commented out unupdated controllers

--- app/controllers/fav_recipes_controller.rb
+++ app/controllers/fav_recipes_controller.rb
@@ -1,17 +1,17 @@
 class FavRecipesController < ApplicationController
 
   def index
-    @user = User.find(params[:userId])
-    recipes = []; 
-    @user.fav_recipes.each do |i|
-      if i.recipe[:yummily_id != nil] 
-        recipes << {
-          name: i.recipe[:name],
-          preparation: i.recipe[:preparation]
-        }
-      end
-    end
-    json_response(recipes)
+    # @user = User.find(params[:userId])
+    # recipes = []; 
+    # @user.fav_recipes.each do |i|
+    #   if i.recipe[:yummily_id != nil] 
+    #     recipes << {
+    #       name: i.recipe[:name],
+    #       preparation: i.recipe[:preparation]
+    #     }
+    #   end
+    # end
+    # json_response(recipes)
   end
 
   def create
