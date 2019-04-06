a5e48738db6c278e6192d8dd0e3a56fdfac77aad
Patrick <padraigtoc@gmail.com>
2019-04-06 12:50:56 -0700

    Recipe page now displaying images/steps

--- client/src/components/RecipePage/RecipePage.jsx
+++ client/src/components/RecipePage/RecipePage.jsx
@@ -14,18 +14,20 @@ class RecipePage extends Component {
       recipeCookTime: "",
       recipeTotalTime: "",
       image: "",
-      recipeIngredients: []
+      recipeIngredients: [], 
+      recipeSteps: ""
     }; 
 
   }
   componentDidMount() {
     getRecipeID(this.props.match.params.id, (res) => {
-      //console.log("Here is the image: " + ))
+      console.log("Here are the steps: " + res.data.attribution.url)
       this.setState({
         recipeTitle: res.data.name,
         recipePrepTime: "Prep time: " + res.data.prepTime,
         recipeCookTime: "Cook time: " + res.data.cookTime,
         recipeTotalTime: "Total time: " + res.data.totalTime,
+        recipeSteps: res.data.attribution.url,
         image: res.data.images[0].hostedLargeUrl,
         recipeIngredients: res.data.ingredientLines
       }) 
@@ -45,6 +47,8 @@ class RecipePage extends Component {
       <br />
       <h4>Ingredients Required:</h4>
       <p> {this.state.recipeIngredients}</p>
+      <h4> Steps: </h4>
+      <p> Find the steps for this recipe <a href={this.state.recipeSteps}>here.</a></p>
       </div>
     )}
 
