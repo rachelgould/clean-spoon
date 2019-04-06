172f79d4835f5cf8c1a23caffcc10ab6651516bb
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-06 12:53:53 -0700

    basic form complete

---
+++ client/src/components/RecipeResults/FilterForm.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { Button, Form, FormGroup, Label, Input, CustomInput } from 'reactstrap';
+
+function FilterForm(props) {
+  const [extraAllergies, setExtraAllergies] = useState([null]);
+  const [diet, setDiet] = useState({
+    vegetarian: false, //Change this so it takes from the server
+    vegan: false,
+  });
+  const [excludedCourses, setExcludedCourses] = useState([null]);
+  const [excludeAllHolidays, setExcludeAllHolidays] = useState(false);
+
+  return (
+    <Form>
+      <FormGroup>
+        <Label for="allergies">Extra ingredients to exclude</Label>
+        <Input name="allergies" id="allergies" placeholder="Start typing and press enter" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="diet">Change your diet</Label>
+        <CustomInput type="switch" id="diet-vegetarian" name="diet-vegetarian" label="Vegetarian" />
+        <CustomInput type="switch" id="diet-vegan" name="diet-vegan" label="Vegan" />
+      </FormGroup>
+      <Label>Excluded courses</Label>
+      <FormGroup check>
+        <Label check>
+          <Input type="checkbox" id="main-dishes" />{' '}
+          Main Course
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="dessert-dishes" />{' '}
+          Dessert
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="side-dishes" />{' '}
+          Side Dish
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="lunch-snack-dishes" />{' '}
+          Lunch & Snacks
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="appetizer-dishes" />{' '}
+          Appetizer
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="salad-dishes" />{' '}
+          Salad
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="bread-dishes" />{' '}
+          Bread
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="breakfast-dishes" />{' '}
+          Breakfast & Brunch
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="soup-dishes" />{' '}
+          Soup
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="beverage-dishes" />{' '}
+          Beverages
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="sauces-dishes" />{' '}
+          Sauce
+        </Label>
+        <Label check>
+          <Input type="checkbox" id="cocktail-dishes" />{' '}
+          Cocktails
+        </Label>
+      </FormGroup>
+      <FormGroup>
+        <CustomInput type="switch" id="exclude-holidays" name="exclude-holidays" label="Exclude holiday dishes?" />
+      </FormGroup>
+      <Button color="danger">Search Again</Button>
+    </Form>
+  );
+}
+
+export default FilterForm;
