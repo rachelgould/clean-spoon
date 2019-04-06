cbb7ce9c65cac1aeac5b8f3f2caa3075aa9100a6
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-05 18:57:56 -0700

    responsive like icons

--- client/src/components/RecipeResults/LikeButton.jsx
+++ client/src/components/RecipeResults/LikeButton.jsx
@@ -3,12 +3,22 @@ import React, { useState } from 'react';
 function LikeButton(props) {
   let { recipe } = props
   const [liked, setLiked] = useState(false);
-  if (liked) {
-    return (<i className="material-icons md-48">star</i>)
-  } else {
-    return (<i className="material-icons md-48">star_border</i>)
+  const [hover, setHover] = useState(false);
+  const toggleHover = () => {
+    setHover(!hover)
   }
-  ;
+  if (hover && liked) {
+    return (<i className="material-icons md-48" onMouseOver={toggleHover} onMouseOut={toggleHover}>star_border</i>)
+  }
+  if (hover && !liked) {
+    return (<i className="material-icons md-48" onMouseOver={toggleHover} onMouseOut={toggleHover}>star</i>)
+  }
+  if (!hover && liked) {
+    return (<i className="material-icons md-48" onMouseOver={toggleHover} onMouseOut={toggleHover}>star</i>)
+  }
+  if (!hover && !liked) {
+    return (<i className="material-icons md-48" onMouseOver={toggleHover} onMouseOut={toggleHover}>star_border</i>)
+  }
 }
 
 export default LikeButton;
