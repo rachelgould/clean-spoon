17e124853817001c8794819734dea2729ae71ec0
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-06 22:25:39 -0700

    mobile nav styles

--- client/src/components/Navbar/nav.jsx
+++ client/src/components/Navbar/nav.jsx
@@ -14,29 +14,14 @@ import './logo.png'
 class nav extends React.Component {
   constructor(props) {
     super(props);
-
-    // this.toggle = this.toggle.bind(this);
-    // this.state = {
-    //   isOpen: false,
-    //   modal: false
-    // };
-    // this.toggle = this.toggle.bind(this);
   }
-  // toggle() {
-  //   this.setState({
-  //     isOpen: !this.state.isOpen,
-  //      modal: !prevState.modal
-  //   });
-  // }
 
   render() {
     return (
       <div className="navigation">
         <Navbar expand="md">
           <NavbarBrand href="/"><img src={require('./logo.png')} alt="Clean Spoon logo" width="200" /></NavbarBrand>
-          {/* <NavbarToggler onClick={this.toggle} /> */}
-          {/* <Collapse isOpen={this.state.isOpen} navbar> */}
-            <Nav className="ml-auto" navbar>
+            <Nav navbar>
               <NavItem>
                 <NavLink href="/fridge"><p className="links">Fridge</p></NavLink>
               </NavItem>
