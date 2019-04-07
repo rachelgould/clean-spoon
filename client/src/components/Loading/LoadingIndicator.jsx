aa6a553169e4b7df6c73dc66fa5935f2ac54add2
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-06 23:05:43 -0700

    styled loading screen

--- client/src/components/Loading/LoadingIndicator.jsx
+++ client/src/components/Loading/LoadingIndicator.jsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 const LoadingIndicator = (props) => {
   return(
   <div className="loading-indicator">
-    <h3>Loading {props.page}</h3>
-    <iframe src="https://gifer.com/embed/767x" allowFullScreen></iframe>
+    {/* <h3>Loading {props.page}</h3> */}
+    <img src="https://media.giphy.com/media/3o7TKUGccxlaQqdgRO/source.gif"></img>
   </div>
   )
 }
