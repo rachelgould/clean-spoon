997c75f650b5dc753aec09df389df18e344b2ce3
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-08 13:33:14 -0700

    add prep time in hours if minutes >= 60

--- client/src/components/RecipeResults/RecipeCard.jsx
+++ client/src/components/RecipeResults/RecipeCard.jsx
@@ -5,13 +5,16 @@ import LikeButton from './LikeButton';
 function RecipeCard(props) {
   let { recipeName, id, course, ingredients, rating, source, image, prepTime } = props.recipe
   let prepTimeInMins = Math.ceil(prepTime/60)
+  if (prepTimeInMins >= 60) {
+    let prepTimeInHrs = prepTimeInMins/60 + 'hours'
+  }
   return (
     <div className="recipes-results-card">
       <Card>
         <CardImg top width="100%" src={image} alt={recipeName} />
         <CardBody>
           <CardTitle>{recipeName}</CardTitle>
-          <CardSubtitle className="small"><p>Prep Time: {prepTimeInMins} mins</p></CardSubtitle>
+          <CardSubtitle className="small"><p>Prep Time: {prepTimeInHrs ? prepTimeInHrs : prepTimeInMins + "mins"}</p></CardSubtitle>
           <CardText className="small">
             <p><strong>Your Ingredients:</strong> Placeholder</p>
             <p><strong>Not in Fridge:</strong> Placeholder</p>
