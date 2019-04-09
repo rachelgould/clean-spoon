8fbf70abbf7a8f939d189303c34e756a302c577d
Tristan <tristanberezowski@gmail.com>
2019-04-09 00:32:37 +0000

    Routes work for fav_recipes

--- app/controllers/fav_recipes_controller.rb
+++ app/controllers/fav_recipes_controller.rb
@@ -1,23 +1,23 @@
 class FavRecipesController < ApplicationController
 
   def index
-    # @user = User.find(params[:userId])
-    # recipes = []; 
-    # @user.fav_recipes.each do |i|
-    #   if i.recipe[:yummily_id != nil] 
-    #     recipes << {
-    #       name: i.recipe[:name],
-    #       preparation: i.recipe[:preparation]
-    #     }
-    #   end
-    # end
-    # json_response(recipes)
+    user = User.find(params[:userId])
+    recipes = [];
+    user.fav_recipes.each do |i|
+      result = JSON.parse(yummly_get(i.yummly_id))
+      result.delete("nutritionEstimates")
+      recipes << result
+    end
+    json_response(recipes)
   end
 
   def create
+    user = User.find(params[:userId])
+    user.fav_recipes.create(yummly_id: params[:recipeId]) ? json_response("Created!") : json_response("Failed to create")
   end
 
   def destroy
+    FavRecipe.destroy(params[:favRecipeId])
   end
 
 end
