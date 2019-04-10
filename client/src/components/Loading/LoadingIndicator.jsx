9271b2a4fea35f11aa27da0c2957ce5baa625713
Rachel Gould <rachegould@gmail.com>
2019-04-10 08:07:51 +0000

    Working on filter form

--- client/src/components/Loading/LoadingIndicator.jsx
+++ client/src/components/Loading/LoadingIndicator.jsx
@@ -4,7 +4,7 @@ const LoadingIndicator = (props) => {
   return(
   <div className="loading-indicator">
     <h1>fire up the oven and let's get cookin'</h1>
-    <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+    <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
   </div>
   )
 }
--- client/src/components/RecipeResults/FilterForm.jsx
+++ client/src/components/RecipeResults/FilterForm.jsx
@@ -2,19 +2,51 @@ import React, { useState } from 'react';
 import { Button, Form, FormGroup, Label, Input, CustomInput } from 'reactstrap';
 
 function FilterForm(props) {
-  const [extraAllergies, setExtraAllergies] = useState([null]);
+  const [extraAllergies, setExtraAllergies] = useState([]);
   const [diet, setDiet] = useState({
-    vegetarian: false, //Change this so it takes from the server
-    vegan: false,
+    vegetarian: false,
+    vegan: false
   });
-  const [excludedCourses, setExcludedCourses] = useState([null]);
+  const [excludedCourses, setExcludedCourses] = useState([]);
   const [excludeAllHolidays, setExcludeAllHolidays] = useState(false);
 
   const allergySubmit = (event) => {
     event.persist();
     event.preventDefault();
-    const { value } = event.target;
-    console.log("Submitted new allergy: ", value)
+    const currentAllergies = [...extraAllergies]
+    const newAllergies = currentAllergies.push(event.target[0].value)
+    setExtraAllergies(newAllergies)
+    event.target[0].value = ''
+  }
+
+  const changeSwitch = (event) => {
+    event.persist();
+    switch (event.target.id) {
+      case 'diet-vegetarian':
+        let currentVegetarian = diet.vegetarian
+        setDiet({vegetarian: !currentVegetarian})
+        break;
+      case 'diet-vegan':
+        let currentVegan = diet.vegan
+        setDiet({vegan: !currentVegan})
+        break;
+      case 'exclude-holidays':
+        let currentExcludeHolidays = excludeAllHolidays
+        setExcludeAllHolidays(!currentExcludeHolidays)
+        break;
+    }
+    console.log("event is::::: ", event)
+    console.log("event.target is::::: ", event.target)
+    console.log("event.targetid is::::: ", event.target.id)
+    console.log("event.value is::::: ", event.target.value)
+  }
+
+  const clickTick = (event) => {
+    event.persist();
+    console.log("event is::::: ", event)
+    console.log("event.target is::::: ", event.target)
+    console.log("event.targetid is::::: ", event.target.id)
+    console.log("event.value is::::: ", event.target.value)
   }
 
   return (
@@ -24,19 +56,19 @@ function FilterForm(props) {
         <FormGroup>
             <Input name="allergies" id="allergies" placeholder="e.g. Brussel Sprouts" />
             <Button color="danger" type="submit">Add Item</Button>
-            <div className="submitted-allergies">{extraAllergies}</div>
+            <div className="submitted-allergies"><p>{extraAllergies}</p></div>
         </FormGroup>
       </Form>
       <Form>
         <FormGroup>
           <Label for="diet">Change your diet</Label>
-          <CustomInput type="switch" id="diet-vegetarian" name="diet-vegetarian" label="Vegetarian" />
-          <CustomInput type="switch" id="diet-vegan" name="diet-vegan" label="Vegan" />
+          <CustomInput type="switch" id="diet-vegetarian" name="diet-vegetarian" label="Vegetarian" onChange={changeSwitch} value={diet.vegetarian ? 'on' : 'off'}/>
+          <CustomInput type="switch" id="diet-vegan" name="diet-vegan" label="Vegan" onChange={changeSwitch} value={diet.vegan ? 'on' : 'off'}/>
         </FormGroup>
         <Label>Excluded courses</Label>
         <FormGroup check>
           <Label check>
-            <Input type="checkbox" id="main-dishes" />{' '}
+            <Input type="checkbox" id="main-dishes" onChange={clickTick}/>{' '}
             Main Course
           </Label>
           <Label check>
@@ -48,8 +80,12 @@ function FilterForm(props) {
             Side Dish
           </Label>
           <Label check>
-            <Input type="checkbox" id="lunch-snack-dishes" />{' '}
-            Lunch & Snacks
+            <Input type="checkbox" id="lunch" />{' '}
+            Lunch
+          </Label>
+          <Label check>
+            <Input type="checkbox" id="snacks" />{' '}
+            Snacks
           </Label>
           <Label check>
             <Input type="checkbox" id="appetizer-dishes" />{' '}
@@ -85,10 +121,11 @@ function FilterForm(props) {
           </Label>
         </FormGroup>
         <FormGroup>
-          <CustomInput type="switch" id="exclude-holidays" name="exclude-holidays" label="Exclude holiday dishes?" />
+          <CustomInput type="switch" id="exclude-holidays" name="exclude-holidays" label="Exclude holiday dishes?" value={excludeAllHolidays ? 'on' : 'off'}/>
         </FormGroup>
         <Button color="danger">Search Again</Button>
       </Form>
+      <p>This is state. Extra allergies: {extraAllergies}. Diet: {diet.vegetarian} and {diet.vegan}, excludedCourses: {excludedCourses}, excludeAllHolidays: {excludeAllHolidays}</p>
     </>
   );
 }
